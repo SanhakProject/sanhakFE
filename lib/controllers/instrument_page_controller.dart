756c25221b7a866de6ce9270c1c335385fe9c167
47813 <vanguard0956@gmail.com>
2025-07-18 18:59:08 +0700

    [FEAT]: 악기 이름 가져오는 기능 변경

--- lib/controllers/instrument_page_controller.dart
+++ lib/controllers/instrument_page_controller.dart
@@ -1,7 +1,12 @@
 import 'package:get/get.dart';
+import 'package:sanhak/views/check/check_decibel_screen.dart';
 
 class InstrumentPageController extends GetxController {
   Rx<String> instrumentName = ''.obs;
   Rx<String> songName = ''.obs;
 
+  void getInstrumentName(String instrument) {
+    instrumentName.value = instrument;
+    Get.to(() => CheckDecibelScreen(), duration: Duration(milliseconds: 300));
+  }
 }
