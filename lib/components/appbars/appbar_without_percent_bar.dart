dd54d52fc748a64551a237b957ab544da53849fb
47813 <vanguard0956@gmail.com>
2025-07-18 18:57:33 +0700

    [FEAT]: 뒤로가기 버튼 활성화

--- lib/components/appbars/appbar_without_percent_bar.dart
+++ lib/components/appbars/appbar_without_percent_bar.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/instrument_page_controller.dart';
 
 class AppBarWithoutPercentBar extends StatelessWidget implements PreferredSizeWidget {
   const AppBarWithoutPercentBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<InstrumentPageController>();
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white,)
+      ),
       title: Row(
         children: [
           Text(
-            "별달거리 - 북",
+            "노래이름 - ${controller.instrumentName.value}",
             style: TextStyle(
               fontSize: 30,
               color: Colors.white,
