eb149ea006ca1fbfa5ff38cd876571e0b9803d0b
47813 <vanguard0956@gmail.com>
2025-08-22 23:36:12 +0900

    [FEAT]: 앱바 퍼센트 바 실시간 반영

--- lib/controllers/music_controller.dart
+++ lib/controllers/music_controller.dart
@@ -4,6 +4,24 @@ import 'package:just_audio/just_audio.dart';
 class MusicController extends GetxController {
   final AudioPlayer _player = AudioPlayer();
 
+  final currentPosition = Duration.zero.obs;
+  final totalDuration = Duration.zero.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    _player.durationStream.listen((duration) {
+      if (duration != null) {
+        totalDuration.value = duration;
+      }
+    });
+
+    _player.positionStream.listen((position) {
+      currentPosition.value = position;
+    });
+  }
+
   Future<void> playMusic() async {
     try {
       await _player.setAsset('assets/dummy/dummy_music.mp3');
@@ -17,5 +35,11 @@ class MusicController extends GetxController {
   void stopMusic() {
     _player.stop();
     _player.dispose();
+  }
+
+  @override
+  void onClose() {
+    _player.dispose();
+    super.onClose();
   }
 }
