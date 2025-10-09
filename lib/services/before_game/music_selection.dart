a6ae442da563d74b9a5da9ad4bdcf2801680f160
jhj04 <hj650272@gmail.com>
2025-10-10 00:33:21 +0900

    [FEAT] : 음악 선택 후 악보 get api 구현

---
+++ lib/services/before_game/music_selection.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import '../../models/track_sheet_grid.dart';
+
+class MusicSelectionService {
+  final Dio _dio = Dio();
+  final String baseUrl = 'v0/';
+
+  Future<TrackSheetGrid> getTrackSheet(int trackId, String instrument) async {
+    try {
+      final response = await _dio.get(
+        '$baseUrl/tracks/$trackId/$instrument',
+      );
+
+      if (response.statusCode == 200) {
+        return TrackSheetGrid.fromJson(response.data);
+      } else {
+        throw Exception('악보를 불러오는 데 실패했습니다.');
+      }
+    } on DioException catch (e) {
+      throw Exception('네트워크 오류 발생: ${e.message}');
+    }
+  }
+}
