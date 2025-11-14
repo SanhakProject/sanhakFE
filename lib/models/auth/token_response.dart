5fd615940ce6b65af2905cd7609d7415dafc5fff
jhj04 <hj650272@gmail.com>
2025-11-15 03:38:39 +0900

    [FEAT] : 리프레시, 엑세스 토큰 관리

---
+++ lib/models/auth/token_response.dart
@@ -0,0 +1,23 @@
+class TokenResponse {
+  final String accessToken;
+  final String refreshToken;
+
+  TokenResponse({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory TokenResponse.fromJson(Map<String, dynamic> json) {
+    return TokenResponse(
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+    };
+  }
+}
