34bcf0133939b13ecca93438d0842eba8e678e92
jhj04 <hj650272@gmail.com>
2025-11-15 04:18:43 +0900

    [FEAT] : 카카오 환경변수 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'components/permissions/permission_handler.dart';
 import 'provider.dart';
@@ -9,6 +10,15 @@ import 'views/login/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 환경변수 로드
+  await dotenv.load(fileName: ".env");
+
+  // 카카오 SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!,
+  );
+
   await SystemChrome.setPreferredOrientations([  /// 화면 가로 고정
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
