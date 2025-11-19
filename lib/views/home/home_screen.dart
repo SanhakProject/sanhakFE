5b5c97fa270f6fe95a7b8340ca4323ad09fe56d9
jhj04 <hj650272@gmail.com>
2025-11-20 00:45:13 +0900

    [FEAT] : 홈 로그아웃 버튼 추가

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'home_button.dart';
+import '../../controllers/login_page_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<LoginPageController>();
+
     return Stack(
       children: [
         Positioned.fill(
@@ -18,13 +22,29 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                '게임이름',
+                '아리노리',
                 style: TextStyle(color: Colors.white, fontSize: 35),
               ),
               HomeButton(),
             ],
           ),
-        )
+        ),
+        // 로그아웃 버튼 (오른쪽 위)
+        Positioned(
+          top: 20,
+          right: 20,
+          child: TextButton(
+            onPressed: () => controller.logout(),
+            child: const Text(
+              '로그아웃',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
